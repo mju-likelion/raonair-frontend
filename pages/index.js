29e480fe728f2219e619525e4b33f8d249cca8db
Bhanjo <hanjo1515@naver.com>
2021-08-27 18:06:36 +0900

    추가: 헤더 추가

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,16 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+// import Head from 'next/head'
+// import styles from '../styles/Home.module.css'
 
+import Image from 'next/image'
 import SetGlobalStyles from '../components/GlobalStyles'
+
+import Header from '../components/Header';
 
 export default function Home() {
   return (
     <>
       <SetGlobalStyles />
+      <Header/>
       <h1>helloworld!</h1>
     </>
   )
