9f5b1ef6cbcb2c71dc4542c5790d41e55125c46f
Bhanjo <hanjo1515@naver.com>
2021-08-30 13:39:26 +0900

    수정: recoil root 위치 변경

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,8 @@
 import '../styles/globals.css'
-import { RecoilRoot } from 'recoil'
 
 function MyApp({ Component, pageProps }) {
   return (
-    <RecoilRoot>
       <Component {...pageProps} />
-    </RecoilRoot>
   );
 }
 
--- pages/index.js
+++ pages/index.js
@@ -5,13 +5,16 @@ import Image from 'next/image'
 import SetGlobalStyles from '../components/GlobalStyles'
 
 import Header from '../components/Header';
+import { RecoilRoot } from 'recoil';
+import HomePage from './HomePage';
 
 export default function Home() {
   return (
-    <>
+    <RecoilRoot>
       <SetGlobalStyles />
       <Header/>
+      <HomePage />
       <h1>helloworld!</h1>
-    </>
+    </RecoilRoot>
   )
 }
