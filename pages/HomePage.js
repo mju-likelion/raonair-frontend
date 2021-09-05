c6fedcf59d926463a9aaf75386468523c83173d6
Bhanjo <hanjo1515@naver.com>
2021-09-05 16:23:42 +0900

    추가: 홈페이지 기능 추가

--- pages/HomePage.js
+++ pages/HomePage.js
@@ -1,29 +1,41 @@
+import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
 import SearchComponent from '../components/SearchComponent';
 
-const Intro = styled.div`
-  /* display: flex; */
-  margin: 99px auto;
-  width: 800px;
-  height: 143px;
-  /* background-color: gray; */
-  text-align: center;
-  p {
-    color: #49b0ff;
+const MainContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  /* max-width: 1280px; */
+  margin: 0 auto;
+  .intro {
+    margin: 99px auto;
+    max-width: 800px;
+    height: 143px;
+    text-align: center;
+    p {
+      color: #49b0ff;
+    }
+    ul {
+      display: flex;
+      justify-content: center;
+      list-style: none;
+    }
+    li {
+      margin: 0 12px;
+      cursor: pointer;
+    }
+    hr {
+      width: 100%;
+      color: #BFBFBF;
+    }
   }
-  ul {
-    display: flex;
-    justify-content: center;
-    list-style: none;
-  }
-  li {
-    margin: 0 12px;
-    cursor: pointer;
-  }
-  hr {
-    width: 100%;
-    color: #BFBFBF;
+`;
+
+const Section = styled.div`
+  height: 645px;
+  .indie {
+    background-color: red;
   }
 `;
 const Theame = styled.h4`
@@ -32,33 +44,85 @@ const Theame = styled.h4`
   margin-bottom: 13px;
 `;
 
-const HomePage = () => {
+const HomePage = ({ plays }) => {
   return (
     <>
       <SearchComponent />
-      <Intro>
-        <Theame>다양한 공연을 라온에어와 함께 만나보세요</Theame>
-        <ul>
-          <li>
-            <p>#인디</p>
-          </li>
-          <li>
-            <p>#신생극단</p>
-          </li>
-          <li>
-            <p>#랭킹</p>
-          </li>
-          <li>
-            <p>#진행중</p>
-          </li>
-        </ul>
-        <hr />
-      </Intro>
-      <Theame>
-        INDIE
-      </Theame>
+      <MainContainer>
+        <div className='intro'>
+          <Theame>다양한 공연을 라온에어와 함께 만나보세요</Theame>
+          {/* 클릭으로 포커스를 맞출지, 페이지 이동을 할지 미정 */}
+          <ul>
+            <li>
+              <p>#인디</p>
+            </li>
+            <li>
+              <p>#신생극단</p>
+            </li>
+            <li>
+              <p>#랭킹</p>
+            </li>
+            <li>
+              <p>#진행중</p>
+            </li>
+          </ul>
+          <hr />
+        </div>
+        <Section>
+          <div className='indie'>
+            <Theame>
+              INDIE
+            </Theame>
+            <ul>
+              { plays && plays.map(play =>
+                <li key={play.id}>{play.title}</li>) }
+            </ul>
+          </div>
+        </Section>
+        <Section>
+          <Theame>
+            NEW TEAM&apos;S PLAY
+          </Theame>
+        </Section>
+        <Section>
+          <Theame>
+            RANKING
+          </Theame>
+        </Section>
+        <Section>
+          <Theame>
+            PROCEEDING
+          </Theame>
+        </Section>
+      </MainContainer>
     </>
   );
 };
 
+
+export async function getStaitcProps() {
+  // eslint-disable-next-line no-console
+  console.log('data fetch@@@@@@@@@@@@@@@@@@@')
+  
+  // 임시적 api
+  const res = await fetch('http://3.38.8.220/api/search/play');
+  const plays = await res.json()
+
+  // eslint-disable-next-line no-console
+  console.log(plays)
+  
+  return {
+    props: { plays }
+  }
+}
+
 export default HomePage;
+
+HomePage.propTypes = {
+  plays: PropTypes.objectOf(
+    PropTypes.oneOfType([
+      PropTypes.string,
+      PropTypes.objectOf(PropTypes.booleanValue),
+    ]),
+  ).isRequired,
+};
