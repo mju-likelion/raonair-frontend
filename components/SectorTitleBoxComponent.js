fd427a6712bb9321c8a79709b49f1156ad9fe128
Bhanjo <hanjo1515@naver.com>
2021-11-02 22:38:24 +0900

    추가: 페이지 섹터 타이틀 컴포넌트 추가

---
+++ components/SectorTitleBoxComponent.js
@@ -0,0 +1,31 @@
+import styled from 'styled-components';
+
+const SectorTitleBox = styled.div`
+  display: flex;
+  margin-top: 58px;
+  &:after, &:before{
+    content: '';
+    background: #529acc;
+    width: 150px;
+    height: 5px;
+    font-size: 0;
+    line-height: 0;
+    margin: 19px;
+  }
+`;
+
+const SectorTitle = styled.p`
+  font-size: 36px;
+  font-weight: bold;
+  color: #222222;
+`;
+
+const SectorTitleBoxComponent = ({ children }) => {
+  return (
+    <SectorTitleBox>
+      <SectorTitle>{children}</SectorTitle>
+    </SectorTitleBox>
+  );
+}
+
+export default SectorTitleBoxComponent;
