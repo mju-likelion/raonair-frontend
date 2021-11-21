11c89fa6e5898f53115adc078bdad092f1abe09d
Bhanjo <hanjo1515@naver.com>
2021-11-22 01:39:23 +0900

    스타일: 코멘트 리스트 별점 추가, 레이아웃 적용

--- components/Comment.js
+++ components/Comment.js
@@ -1,7 +1,5 @@
 import styled from 'styled-components';
 
-import Star from './Star';
-
 const CommentBox = styled.div`
   display: block;
   width: 1140px;
@@ -21,20 +19,46 @@ const CommentItem = styled.div`
   border-radius: 6px;
   border: 1px #87cbff solid;
   background-color: #f9f9f9;
+  font-size: 30px;
+`;
+
+const CommentInnerTop = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 19px;
+`;
+
+const StarBox = styled.div`
+  width: 250px;
+  height: 50px;
+  background: url('../svg/star_default.svg');
+`;
+
+const Star = styled.div`
+  width: ${(props) => props.rate && `${props.rate}px`};
+  height: 50px;
+  background: url('../svg/star.svg');
 `;
 
 const Commnet = ({ comments }) => {
   return (
     // 후기가 페이징 구현 필요
     <CommentBox>
-      {comments.map(comment =>
+      {comments.map((comment) => (
         <CommentItem key={comment.id}>
-          {/* 별을 어떻게 반복해야하는지 고민 필요 */}
-          {/* {comment.star} */}
-          <p>{comment.nickname} | {comment.date}</p>
+          <CommentInnerTop>
+            {/* StarBox에 기본 이미지를 주고 평점에 따라 star 이미지를 덮어씌움 */}
+            <StarBox>
+              <Star rate={comment.star * 50} />
+            </StarBox>
+            <p>
+              {comment.nickname} | {comment.date}
+            </p>
+          </CommentInnerTop>
           <p>{comment.content}</p>
         </CommentItem>
-      )}
+      ))}
     </CommentBox>
   );
 };
